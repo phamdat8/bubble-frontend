87ca03096da20897c8bab4bc4fd5c5de037fa630
phamdat8 <46599935+phamdat8@users.noreply.github.com>
2021-09-06 19:39:14 +0700

    Update Home.vue
    test

--- src/components/client/Home.vue
+++ src/components/client/Home.vue
@@ -1,9 +1,15 @@
 <template>
     <div id="home">
         <div class="background">
-
+            <div class="row">
+                <div class="col-10 p-5">hello</div>
+                <button class="col-2 btn btn-ouline-primary">Hello</button>
+                <div class="col-4">hello 2</div>
+            </div>
         </div>
-
+        <div>
+            
+        </div>
     </div>
 </template>
 <script>
