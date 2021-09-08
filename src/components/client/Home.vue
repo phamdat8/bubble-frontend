e3b6b6b4e709f312d7b758a669d5b63d36ce246f
leduyen8 <90141497+leduyen8@users.noreply.github.com>
2021-09-08 21:21:05 +0700

    insert_layout
    insert_layout

--- src/components/client/Home.vue
+++ src/components/client/Home.vue
@@ -2,11 +2,42 @@
     <div id="home">
         <div class="background">
 <div class="row">
-                <div  class="col-4" style="padding-left: 300px;">
-                    <img src="static\images\bubble1.jpg" >
+                
+                <nav class="navbar navbar-fixed-bottom  row p-5">
+                    <h2 class="m-0" style="text-align:center;"><b>Lí do bạn nên cùng học với Bubble</b></h2>
+                <div class="col-4" style="padding-left:250px;">
+                   <img src="static\images\fire.png" style="width:25px; height:25px;">
+                   <h5>Tiến bộ nhanh chóng</h5>
+                    <div class=" mt-0">
+                       Các nghiên cứu đã chứng minh các khóa học của chúng tôi giảng dạy các kỹ năng nghe, nói và đọc một cách hiệu quả rõ rệt.
+                    </div>
+                    <img src="static\images\tick.png" style="width:25px; height:25px;padding-right:300px;">
+                   <h5>Cá nhân hóa trải nghiệm học tập</h5>
+                    <div class=" mt-0">
+                     Sự kết hợp hoàn hảo giữa trí thông minh nhân tạo (AI) và khoa học ngôn ngữ, tạo ra các bài học cá nhân hóa giúp mọi người học ở đúng tiến độ và cấp độ phù hợp.
+                    </div>
+                </div>
+                <div class="col-3">
+                     <img src="static\images\c2.png" style="width:400px; height:400px;padding-right:50px;">
+                </div>
+                <div class="col-4"style="padding-right:250px;">
+                   <img src="static\images\king.png" style="width:25px; height:25px;">
+                   <h5>Truyền động lực học</h5>
+                    <div class=" mt-0">
+                       Chúng tôi giúp người học dễ dàng xây dựng thói quen học tập, qua những tính năng mô phỏng trò chơi, các thử thách vui vẻ, và nhắc nhở từ linh vật thân thiện - Bubble.
+                    </div>
+                    <img src="static\images\book.png" style="width:25px; height:25px;">
+                   <h5>Học thật vui!</h5>
+                    <div class=" mt-0">
+                     Học hiệu quả mà không nhàm chán! Xây dựng kỹ năng mỗi ngày với các bài học thú vị cùng các nhân vật ngộ nghĩnh.
+                    </div>
+                </div>
+        </nav>
+                <div  class="col-4" style="padding-left: 300px;padding-top:20px;">
+                    <img src="static\images\b1.png" >
                  </div>
 
-                 <div class="col-8" style="padding-top:80px;" >
+                 <div class="col-8" style="padding-top:80px;padding-left: 100px;" >
                         <h3>Tăng tốc việc học của bạn với Bubble nào!</h3>
                         <div class="a1">
                             Việc học ngôn ngữ trên Duolingo là hoàn toàn miễn phí.
@@ -22,16 +53,16 @@
                 </div>
 
                 <div class="col-4 text-center">
-                    <img src="static\images\bubble1.jpg"style="padding-right: 200px;">
+                    <img src="static\images\b2.png"style="padding-right:300px;">
                 </div>
                 <div>
                 <div class="col-8  d1"></div>
                 </div>
                 <div  class="col-4" style="padding-left: 300px;">
-                    <img src="static\images\bubble1.jpg" >
+                    <img src="static\images\b3.png" >
                  </div>
 
-                 <div class="col-8" style="padding-top:80px;" >
+                 <div class="col-8" style="padding-top:80px;padding-left:80px;" >
                         <h2>Bubble với kho tàng kiến thức!</h2>
                         <div class="a1">
                             Với hàng trăm đề thi và các kiến thực liên tục được cập nhật.
@@ -49,7 +80,7 @@
 </script>
 <style>
     .background{
-        background-color: #fafafa;
+        background-color: hsl(0, 0%, 100%);
     }
     .d1{
         border-bottom: 2px solid #bbb5b5;
