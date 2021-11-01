d84ba6da31b83243e3cdaaf3b4347e27b5443b43
phamdat8 <DatPT@bestarion.com>
2021-11-01 10:37:21 +0700

    A

---
+++ src/components/client/Login.vue
@@ -0,0 +1,159 @@
+<template>
+    <div id="login" class="d-flex justify-content-center row">
+        <div class="col-4 mt-5">
+            <h1><b> Đăng nhập bubble</b></h1>
+            <form v-on:submit.prevent="submitForm">
+                <div class="group">      
+                    <input type="text white" required v-model="form.username">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Tài khoản</label>
+                </div>
+                <div class="group">      
+                    <input type="password" required v-model="form.password">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Mật khẩu</label>
+                </div>
+                
+                <div class="text-center">
+                    <button class="btn btn-outline-primary" type="submit"> Đăng nhập </button>
+                </div>
+            </form>
+        </div>
+        
+    </div>
+</template>
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'Login',
+        data(){
+            return{
+                form: {
+                    username: '',
+                    password: '',
+                    
+                }
+            }
+        },
+        methods:{
+        submitForm(){
+            axios.post(this.$api+'/session', this.form)
+                .then((res) => {
+                    this.$notify({
+                        title: res.data.title,
+                        type: res.data.status,
+                        text: res.data.message
+                    });
+                    if(res.data.status == 'success'){
+                        localStorage.login = true;
+                        localStorage.username = res.data.data[0]["username"];
+                        localStorage.name = res.data.data[0]["name"];
+                        localStorage.user_id = res.data.data[0]["id"];
+                        this.$router.push("/user/info");
+                        
+                    }
+                }
+            );
+        }
+    }
+    }
+</script>
+<style>
+    .group 			  { 
+    position:relative; 
+    margin-bottom:45px; 
+    }
+    input 				{
+    font-size:18px;
+    padding:10px 10px 10px 5px;
+    display:block;
+    width:100%;
+    border:none;
+    border-bottom:1px solid #757575;
+    }
+    .white{
+        background-color: white;
+    }
+    input:focus 		{ outline:none; }
+
+    /* LABEL ======================================= */
+    label 				 {
+    color:#999; 
+    font-size:18px;
+    font-weight:normal;
+    position:absolute;
+    pointer-events:none;
+    left:5px;
+    top:10px;
+    transition:0.2s ease all; 
+    -moz-transition:0.2s ease all; 
+    -webkit-transition:0.2s ease all;
+    }
+
+    /* active state */
+    input:focus ~ label, input:valid ~ label 		{
+    top:-20px;
+    font-size:20px;
+    font-weight: bold;
+    color: black;
+    }
+
+    /* BOTTOM BARS ================================= */
+    .bar 	{ position:relative; display:block; width:100%; }
+    .bar:before, .bar:after 	{
+    content:'';
+    height:2px; 
+    width:0;
+    bottom:1px; 
+    position:absolute;
+    background:#5264AE; 
+    transition:0.2s ease all; 
+    -moz-transition:0.2s ease all; 
+    -webkit-transition:0.2s ease all;
+    }
+    .bar:before {
+    left:50%;
+    }
+    .bar:after {
+    right:50%; 
+    }
+
+    /* active state */
+    input:focus ~ .bar:before, input:focus ~ .bar:after {
+    width:50%;
+    }
+
+    /* HIGHLIGHTER ================================== */
+    .highlight {
+    position:absolute;
+    height:60%; 
+    width:100px; 
+    top:25%; 
+    left:0;
+    pointer-events:none;
+    opacity:0.5;
+    }
+
+    /* active state */
+    input:focus ~ .highlight {
+    -webkit-animation:inputHighlighter 0.3s ease;
+    -moz-animation:inputHighlighter 0.3s ease;
+    animation:inputHighlighter 0.3s ease;
+    }
+
+    /* ANIMATIONS ================ */
+    @-webkit-keyframes inputHighlighter {
+        from { background:#5264AE; }
+    to 	{ width:0; background:transparent; }
+    }
+    @-moz-keyframes inputHighlighter {
+        from { background:#5264AE; }
+    to 	{ width:0; background:transparent; }
+    }
+    @keyframes inputHighlighter {
+        from { background:#5264AE; }
+    to 	{ width:0; background:transparent; }
+    }
+</style>
---
+++ src/components/client/user/Info.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="user-info" class="m-4">
+        <div class="row">
+            <div class="col-3 text-center">
+                <div class="img-info">
+                    <img v-bind:src="avatar" height="100%">
+                </div>
+            </div>
+            <div class="col-9 p-3">
+                <h4>{{ username }}</h4>
+                <p><b>{{ name }}</b></p>
+            </div>
+        </div>   
+    </div>
+</template>
+<script>
+    export default {
+        name: 'UserInfo',
+        data(){
+            return{
+                avatar: null,
+                username: localStorage.username,
+                name: localStorage.name,
+            }
+        },
+        mounted() {
+            this.avatar = this.$file + '/users/' + localStorage.user_id + '.png';
+            if (localStorage.getItem('reloaded')) {
+                localStorage.removeItem('reloaded');
+            } else {
+                localStorage.setItem('reloaded', '1');
+                location.reload();
+            }
+        },
+    }
+</script>
+<style>
+    .img-info img{
+        width:200px;
+        height:200px;
+        border-radius: 50%;
+        border:2px solid black;
+    }
+</style>
